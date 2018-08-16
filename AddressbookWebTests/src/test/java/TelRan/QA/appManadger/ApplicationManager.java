1b900ca8f356c3d102e16389e373d85236cda7dc
Olga <furmanovolga@gmail.com>
2018-08-16 09:45:36 +0300

    browser selection

--- AddressbookWebTests/src/test/java/TelRan/QA/appManadger/ApplicationManager.java
+++ AddressbookWebTests/src/test/java/TelRan/QA/appManadger/ApplicationManager.java
@@ -2,6 +2,8 @@ package TelRan.QA.appManadger;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,9 +13,21 @@ public class ApplicationManager {
     SessionHelper sessionHelper;
     GroupHelper groupHelper;
     WebDriver wd;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+
+
+        this.browser = browser;
+    }
 
     public void start() {
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        }
+
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         sessionHelper = new SessionHelper(wd);
--- AddressbookWebTests/src/test/java/TelRan/QA/tests/TestBase.java
+++ AddressbookWebTests/src/test/java/TelRan/QA/tests/TestBase.java
@@ -1,12 +1,13 @@
 package TelRan.QA.tests;
 
 import TelRan.QA.appManadger.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    public static ApplicationManager app = new ApplicationManager();
+    public static ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() {
