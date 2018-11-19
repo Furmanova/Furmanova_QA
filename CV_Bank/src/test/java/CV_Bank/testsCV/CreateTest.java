1ad7099437571c99f4c71f3e7a403d49ca548f28
Olga <furmanovolga@gmail.com>
2018-11-19 08:44:43 +0200

    code simplified

--- CV_Bank/src/test/java/CV_Bank/testsCV/CreateTest.java
+++ CV_Bank/src/test/java/CV_Bank/testsCV/CreateTest.java
@@ -1,53 +1,52 @@
 package CV_Bank.testsCV;
 
+import CV_Bank.appManager.CreateHelper;
 import org.testng.annotations.Test;
 
 public class CreateTest extends TestBase {
     @Test
 
     public void testCreate() {
-        app.getCreateHelper().goToTheCreatePage();
-        app.getCreateHelper().createTitle(
+        CreateHelper helper = app.getCreateHelper();
+        helper.goToTheCreatePage();
+        helper.createTitle(
                 "sdsd",
                 "sdsd",
                 "dsd",
                 "sds",
                 "01022018",
                 "sds");
-        app.getCreateHelper().contactInfo(
+        helper.contactInfo(
                 121333,
                 "wqw",
                 "");
-        app.getCreateHelper().description(
+        helper.description(
                 "df",
                 "sfdsdgf",
                 1,
                 2);
-        app.getCreateHelper().education(
+        helper.education(
                 "sff",
                 "sff",
                 2,
                 "sf");
-        app.getCreateHelper().projectsJobs(
+        helper.projectsJobs(
                 "dfd",
                 "ss",
                 "dsd",
                 2,
                 1);
-        app.getCreateHelper().achievements(
+        helper.achievements(
                 "fg",
                 3);
-        app.getCreateHelper().certifications(
+        helper.certifications(
                 "dhdh",
                 4);
-        app.getCreateHelper().footerLinks(
+        helper.footerLinks(
                 "",
                 "",
                 "");
-        app.getCreateHelper().footerInfo(
-                "");
-
-
+        helper.footerInfo("");
 
         /*LanguagesData languages = new LanguagesData()
                 .setLanguage("");
